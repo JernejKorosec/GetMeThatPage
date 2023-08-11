4abff394318eb1556a65afa78ad3a30231074513
Jernej Korošec <jernejk.alfa@gmail.com>
2023-08-11 20:03:26 +0200

    Loading Index Page.

--- GetMeThatPage/Program.cs
+++ GetMeThatPage/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace GetMeThatPage
+﻿using HtmlAgilityPack;
+
+namespace GetMeThatPage
 {
     internal class Program
     {
+        private static Stream filePath;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Scrap, scrap, get some HTML snack...");
+
+            // From Web
+            var url = "http://books.toscrape.com/";
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+
+            Console.ReadKey();
         }
     }
 }
