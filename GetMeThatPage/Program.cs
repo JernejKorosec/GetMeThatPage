bbda59627c2f9ba2011e695923c6531e3150e4b8
Jernej Korošec <jernejk.alfa@gmail.com>
2023-08-11 19:50:47 +0200

    Add project files.

---
+++ GetMeThatPage/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GetMeThatPage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Scrap, scrap, get some HTML snack...");
+        }
+    }
+}
