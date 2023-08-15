40f123c1d3cd3ed14dcf1de0e760da63eb50582f
Jernej Korošec <jernejk.alfa@gmail.com>
2023-08-15 13:11:17 +0200

    Default Run

--- GetMeThatPage/Program.cs
+++ GetMeThatPage/Program.cs
@@ -4,20 +4,20 @@ namespace GetMeThatPage
 {
     internal class Program
     {
-        private static String webPageUrl = "http://books.toscrape.com/";
-        private static string savePath = AppDomain.CurrentDomain.BaseDirectory;
-
         static void Main(string[] args)
         {
             Console.WriteLine("Scrap, scrap, get some HTML snack...");
-
+            /*
             WebScraper w = WebScraper.Instance.                         // At this point, the save directory for web page is set
                                                                         // and also the web address to scrape the data from.
                 SaveDirectory(AppDomain.CurrentDomain.BaseDirectory).   // Set directory to Save scraped data to
                 WebAddress("www.neobstaja.si").                         // set custom web address to traverse and scrape
                 WebAddress().                                           // reset to the default hardcoded WebAddress
-                Scrape();                                               // start scraping.
-            
+                Scrape();                                               // start scraping using custom or hardcoded values
+            */
+
+            // Suffice, using hardcoded values
+            WebScraper.Instance.Scrape();
             Console.ReadKey();
         }
     }
