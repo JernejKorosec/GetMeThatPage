247b9149dec1ddf33fed418d400b1584e9d61bd6
Jernej Korošec <jernejk.alfa@gmail.com>
2023-08-22 15:56:56 +0200

    Proper Naming

---
+++ GetMeThatPage3/Scraper/ResourceFiles/FilePath.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetMeThatPage3.Scraper.ResourceFiles
+{    /// <summary>
+     /// Properties naming convention
+     /// Remote   is Web
+     /// Local    is Computer (HDD)
+     /// Absolute address from start up to file
+     /// Relative address from arbitrary path to file
+     /// File     if prepended
+     /// Dir     if path is a directory
+     /// </summary>
+    public class FilePath
+    {
+        public string? Filename { get; set; }  // index.html
+        public string? RelativePath { get; set; }      // oscar/kilo/
+        public string? AbsolutePath { get; set; }     // oscar/kilo/index.html
+        public string? getExtension()
+        {
+            return Path.GetExtension(Filename);
+        }
+    }
+}
---
+++ GetMeThatPage3/Scraper/ResourceFiles/ResourceFile.cs
@@ -0,0 +1,25 @@
+﻿using static GetMeThatPage3.Scraper.ResourceFiles.FilePath;
+using HtmlAgilityPack;
+using GetMeThatPage3.Scraper.ResourceFiles;
+using GetMeThatPage3.Helpers.State;
+
+namespace GetMeThatPage2.Helpers.WebOperations.ResourceFiles
+{
+    /// <summary>
+    /// Properties naming convention
+    /// Remote   is Web
+    /// Local    is Computer (HDD)
+    /// Absolute address from start up to file
+    /// Relative address from arbitrary path to file
+    /// File     if prepended
+    /// Dir     if path is a directory
+    /// </summary>
+    public class ResourceFile
+    {
+        public string? AppRoot { get; set; }  // c:\some\where\
+        public string? WebRoot { get; set; } // http://books.toscrape.com
+        public FilePath? Remote { get; set; }
+        public FilePath? Local { get; set; }
+        public State? State { get; set; }
+    }
+}
