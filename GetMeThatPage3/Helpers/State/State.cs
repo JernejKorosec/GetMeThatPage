f1bdff17d6fd14793bb55b3c170d8fe28393488e
Jernej Korošec <jernejk.alfa@gmail.com>
2023-08-22 15:29:43 +0200

    comments

--- GetMeThatPage3/Helpers/State/State.cs
+++ GetMeThatPage3/Helpers/State/State.cs
@@ -2,8 +2,17 @@
 {
     public class State
     {
+        /// <summary>
+        /// Is stream loaded or not
+        /// </summary>
         public bool IsLoaded { get; set; }
+        /// <summary>
+        /// was file processed, parsed
+        /// </summary>
         public bool IsParsed { get; set; }
+        /// <summary>
+        /// is file saved locally
+        /// </summary>
         public bool IsSaved { get; set; }
     }
 }
