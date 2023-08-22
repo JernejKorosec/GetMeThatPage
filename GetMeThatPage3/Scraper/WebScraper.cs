4b0209ef2ecec61985996424f5dc92d8287e650c
Jernej Korošec <jernejk.alfa@gmail.com>
2023-08-22 14:57:43 +0200

    Adding Async Non blockin Concurrent Dictionary Example test.

---
+++ GetMeThatPage3/Scraper/WebScraper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetMeThatPage3.Scraper
+{
+    public class WebScraper
+    {
+        private readonly string? _webPageUrl;
+        public WebScraper(string? webPageUrl)
+        {
+            _webPageUrl = webPageUrl;
+        }
+    }
+}
---
+++ GetMeThatPageTestProject/UnitTest2.cs
@@ -0,0 +1,32 @@
+﻿using GetMeThatPage3.Helpers;
+using GetMeThatPageTestProject.Address;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetMeThatPageTestProject
+{
+    public class Test2
+    {
+        [SetUp]
+        public void Setup() { }
+        [Test]
+        public void Test_ConcurrentBag_Example()
+        {
+            try
+            {
+                
+
+
+            // Assert.That(areStringsJoinedAlsoRelative, Is.EqualTo(isPathRelativeAddress));
+            // Assert.Fail();
+            }
+            catch (Exception)
+            {
+                //Assert.Fail();
+            }
+        }
+    }
+}
